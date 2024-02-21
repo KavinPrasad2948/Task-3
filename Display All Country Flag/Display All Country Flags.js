b956751d194feedb9004bbe0eae7687ffbb7bc5d
KavinPrasad2948 <kavinprasad2948@gmail.com>
2024-02-21 14:54:39 +0530

    completed

---
+++ Display All Country Flag/Display All Country Flags.js
@@ -0,0 +1,9 @@
+const req = new XMLHttpRequest();
+//req.addEventListener("load", reqListener);
+req.open("GET", "https://restcountries.com/v3.1/all");
+req.send();
+req.onload = function() {
+  let Obj = JSON.parse(this.response);
+  for (let i = 0;i< Obj.length;i++) {
+    console.log(Obj[i].flags.png);}}
+
